f29f9952637953ebda76ae69f619a930b8205e32
Joey Bernard <druid.bernard@gmail.com>
2020-01-01 21:07:40 -0400

    * Added a build text file in the doc subdirectory to document building find_tags modules. * Removed the file git-getrevision.sh. Instead, we will use tags to mark version numbering. * Changed 'isnan' to 'std::isnan' in src/Lotek_Data_Source.cpp and src/Tag_Foray.cpp so that they are compliant with C++ 11. * Changed the file src/Makefile so that it uses g++ as the compiler rather than gfilt, so that a standard toolchain is used.

--- git-getrevision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-revisioncount=`git log --oneline | wc -l`
-projectversion=`git describe --tags --long`
-cleanversion=${projectversion%%-*}
-
-#echo "$projectversion-$revisioncount"
-echo "$cleanversion.$revisioncount"
